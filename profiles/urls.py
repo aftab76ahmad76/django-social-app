1910b41632d69bc8ac72ad4309190d5a1b746b3b
Aftab Ahmad <aftab76ahmad76@gmail.com>
2022-10-10 18:13:46 +0500

    changes

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,26 +1,20 @@
 from django.urls import path
-from .views import (
-    my_profile_view,
-    invites_recieved_view,
-    invite_profiles_list_view,
-    ProfileListView,
-    send_invitation,
-    remove_friends,
-    accept_invitation,
-    reject_invitation,
-    ProfileDetailView
-)
+
+from .views import (ProfileDetailView, ProfileListView, accept_invitation,
+                    invite_profiles_list_view, invites_recieved_view,
+                    my_profile_view, reject_invitation, remove_friends,
+                    send_invitation)
 
 app_name = "profiles"
 
 urlpatterns = [
     path("", ProfileListView.as_view(), name="all-profiles-view"),
+    path("accept-invitation/", accept_invitation, name="accept-invitation-view"),
     path("send-invite/", send_invitation, name="send-invite-view"),
     path("myProfile/", my_profile_view, name="my-profile-view"),
     path("my-invites/", invites_recieved_view, name="my-invites-view"),
     path("to-invite/", invite_profiles_list_view, name="to_invite_profiles-view"),
     path("remove-friends/", remove_friends, name="remove-friends-view"),
     path("<slug>/", ProfileDetailView.as_view(), name="profile-detail-view"),
-    path("accept-invitation/", accept_invitation, name="accept-invitation-view"),
     path("reject-invitation/", reject_invitation, name="reject-invitation-view"),
 ]
